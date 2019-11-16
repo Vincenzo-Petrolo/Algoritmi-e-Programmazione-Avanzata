b1b4672cce34fead25b4e933ad0d59f328a5f68f
Vincenzo <vincenzo.petrolo99@gmail.com>
2019-11-16 17:12:36 +0100

    complete

--- Lab06/vertex_cover.c
+++ Lab06/vertex_cover.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define __FILE_NAME__ "vertex.txt"
+#define __FILE_NAME__ "grafo.txt"
 
 typedef struct {
     int u;
@@ -11,18 +11,25 @@ typedef enum {FALSE,TRUE} boolean;
 
 //Tutti i valori sono passati by pointer in maniera da non pagare il costo di copiatura
 
-boolean vertex_cover(arco_non_orientato_t **archi,int **set,int n,int n_vertici);
-void power_set();
-void display_set(int **set,int n);
+boolean vertex_cover(arco_non_orientato_t **archi,int **set,int **sol,int n,int n_archi);
+void powerset(int pos, int *val, int *sol, int k,arco_non_orientato_t **archi,int n_archi);
+void display_set(int **set,int n,int **sol);
 void leggi_archi(arco_non_orientato_t **archi,int *n_vertici,int *n_archi);
-boolean in_vertex(arco_non_orientato_t *archi,int *set,int n_set);
-void powerset(int pos, int *val, int *sol, int k,int start, int count,arco_non_orientato_t **archi,int n_archi);
+boolean appartiene_a_W(arco_non_orientato_t arco,int *W,int dim_W,int *sol);
 
 int main() {
     arco_non_orientato_t *archi;
     int n_vertici,n_archi;
+    int pos=0;
 
     leggi_archi(&archi,&n_vertici,&n_archi); //legge e salva nella struttura dati tutti gli archi
+    int val[n_vertici];
+    int sol[n_vertici];
+    int k = n_vertici;
+    for (int i = 0; i < n_vertici; i++) {
+        val[i] = i;
+    }
+    powerset(pos, val, sol,k,&archi, n_archi);
 
     return 0;
 }
@@ -30,7 +37,7 @@ int main() {
 
 void leggi_archi(arco_non_orientato_t **archi,int *n_vertici,int *n_archi) {
     FILE *fp;
-    arco_non_orientato_t **tmp;
+    arco_non_orientato_t *tmp;
     int u,v;
 
     if ((fp = fopen(__FILE_NAME__,"r")) == NULL) {
@@ -39,55 +46,64 @@ void leggi_archi(arco_non_orientato_t **archi,int *n_vertici,int *n_archi) {
     }
     fscanf(fp,"%d %d",n_vertici,n_archi);
 
-    tmp = (arco_non_orientato_t**) malloc(*n_archi * sizeof(arco_non_orientato_t));
+    tmp = (arco_non_orientato_t*) malloc(*n_archi * sizeof(arco_non_orientato_t));
     if (tmp == NULL) {
         printf("\nERRORE! Heap insufficiente");
         exit(EXIT_FAILURE);
     }
-    archi = tmp;
+    *archi = tmp; //assegno alla variabile puntatore che punta al vettore di struct
 
     for (int i = 0; i < *n_archi; i++) {
         fscanf(fp,"%d %d",&u,&v);
-        archi[i]->u = u;
-        archi[i]->v = v;
+        (*archi)[i].u = u;
+        (*archi)[i].v = v;
     }
     fclose(fp);
 }
 
-void display_set(int **set,int n){
+void display_set(int **set,int n,int **sol){
     printf("\n{");
-    printf("%d",*set[0]);
-    for (int i = 1; i < n; i++) {
-        printf(",%d",*set[i]);
+    for (int i = 0; i < n; i++) {
+        if ((*sol)[i] != 0)
+            printf(" %d",(*set)[i]);
     }
-    printf("}");
+    printf(" }");
 }
 
-boolean vertex_cover(arco_non_orientato_t **archi,int **set,int n,int n_archi){
+boolean vertex_cover(arco_non_orientato_t **archi,int **set,int **sol,int n,int n_archi){
+    boolean vertex_cover = TRUE; //suppongo che  sia un vertex cover
+
     for (int i = 0; i < n_archi;i++) {
-        if (in_vertex(archi[i],*set,n) == FALSE)
-            return FALSE;
+        if (appartiene_a_W((*archi)[i],*set,n,*sol) == FALSE)
+            return vertex_cover=FALSE;
     }
-    return TRUE;
+    return vertex_cover; //Se arriva fino a qua, allora vertex_cover è TRUE
+
 }
 
-boolean in_vertex(arco_non_orientato_t *archi,int *set,int n_set) {
-    for (int i = 0; i < n_set; i++) {
-        if (set[i] != archi->u && set[i] != archi->v)
-            return FALSE;
+boolean appartiene_a_W(arco_non_orientato_t arco,int *W,int dim_W,int *sol){
+    boolean appartenenza = FALSE; //attributo all'arco, suppongo sia falso, nel caso contrario mi ricredo e ritorno TRUE
+
+    for (int i = 0; i < dim_W; i++) {
+        if (sol[i] != 0) {
+            if (arco.u == W[i] || arco.v == W[i]) {
+                return appartenenza = TRUE;
+            }
+        }
     }
-    return TRUE;
+    return appartenenza; //Se arriva fin qui, allora appartenenza è FALSE
 }
 
-void powerset(int pos, int *val, int *sol, int k,int start, int count,arco_non_orientato_t **archi,int n_archi) {
-    int i;
-    if (start >= k) {
-        vertex_cover(archi,&sol,k,n_archi);
-        return;
+void powerset(int pos,int *val, int *sol, int k,arco_non_orientato_t **archi,int n_archi) {
+    boolean result;
+    if (pos >= k) {
+        result=vertex_cover(archi,&val,&sol,k,n_archi);
+        if (result == TRUE)
+            display_set(&val,k,&sol);
+    return;
     }
-    for (i = start; i < k; i++) {
-        sol[pos] = val[i];
-        powerset(pos + 1, val, sol, k, i + 1, count);
-    }
-    powerset(pos, val, sol, k, k, count);
+    sol[pos] = 0;
+    powerset(pos+1, val, sol, k,archi,n_archi);
+    sol[pos] = 1;
+    powerset(pos+1, val, sol, k,archi,n_archi);
 }
